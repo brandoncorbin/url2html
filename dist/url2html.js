e5689ce301dd67d03851b73db232e23dac1eaad6
Brandon Corbin <brandon@icorbin.com>
2018-03-30 09:15:22 -0400

    Updating build

--- dist/url2html.js
+++ dist/url2html.js
@@ -70,60 +70,66 @@
 "use strict";
 
 
-//@flow
+// @flow
 module.exports = function (url) {
   url = url || null;
-  var window;
 
   var link = function link(_passedURL) {
-    console.log("Passed URL", _passedURL);
-    return '<a href="' + _passedURL + '" target="_blank" class="url2html url2html-link">' + _passedURL + "</a>";
+    console.log('Passed URL', _passedURL);
+
+    return `<a href="${_passedURL}" target="_blank" class="url2html url2html-link">${_passedURL}</a>`;
   };
 
   var youtube = function youtube(_passedURL) {
     var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
     var match = _passedURL.match(regExp);
+
     if (match && match[7].length === 11) {
-      return '<iframe class="url2html url2html-youtube"width="560" height="315" src="//www.youtube.com/embed/' + match[7] + '" frameborder="0" allowfullscreen></iframe>';
-    } else {
-      return link(_passedURL);
+      return `<iframe class="url2html url2html-youtube"width="560" height="315" src="//www.youtube.com/embed/${match[7]}" frameborder="0" allowfullscreen></iframe>`;
     }
+
+    return link(_passedURL);
   };
   var vimeo = function vimeo(_passedURL) {
     var regExp = /(http|https):\/\/(www\.)?vimeo.com\/(\d+)($|\/)/;
     var match = _passedURL.match(regExp);
+
     if (match) {
-      return '<iframe class="url2html url2html-vimeo" src="http://player.vimeo.com/video/' + match[2] + '?color=f2bc27" width="500" height="281" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
-    } else {
-      return link(_passedURL);
+      return `<iframe class="url2html url2html-vimeo" src="http://player.vimeo.com/video/${match[2]}?color=f2bc27" width="500" height="281" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>`;
     }
+
+    return link(_passedURL);
   };
   var image = function image(_passedURL) {
-    return '<img class="url2html url2html-image" src="' + _passedURL + '" align="absmiddle" /></span>';
+    return `<img class="url2html url2html-image" src="${_passedURL}" align="absmiddle" /></span>`;
   };
 
   var audio = function audio(_passedURL) {
-    return '<span class="url2html url2html-audio"><object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=10,0,0,0" width="30" height="30"><PARAM NAME=movie VALUE="http://www.strangecube.com/audioplay/online/audioplay.swf?file=' + _passedURL + '&auto=no&sendstop=yes&repeat=0&buttondir=http://www.strangecube.com/audioplay/online/alpha_buttons/negative&bgcolor=0xffffff&mode=playpause"><PARAM NAME=quality VALUE=high><PARAM NAME=wmode VALUE=transparent><embed src="http://www.strangecube.com/audioplay/online/audioplay.swf?file=' + _passedURL + '&auto=no&sendstop=yes&repeat=0&buttondir=http://www.strangecube.com/audioplay/online/alpha_buttons/negative&bgcolor=0xffffff&mode=playpause" quality=high wmode=transparent width="30" height="30" align="" TYPE="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer"></embed></object></span>';
+    return `<span class="url2html url2html-audio"><object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=10,0,0,0" width="30" height="30"><PARAM NAME=movie VALUE="http://www.strangecube.com/audioplay/online/audioplay.swf?file=${_passedURL}&auto=no&sendstop=yes&repeat=0&buttondir=http://www.strangecube.com/audioplay/online/alpha_buttons/negative&bgcolor=0xffffff&mode=playpause"><PARAM NAME=quality VALUE=high><PARAM NAME=wmode VALUE=transparent><embed src="http://www.strangecube.com/audioplay/online/audioplay.swf?file=${_passedURL}&auto=no&sendstop=yes&repeat=0&buttondir=http://www.strangecube.com/audioplay/online/alpha_buttons/negative&bgcolor=0xffffff&mode=playpause" quality=high wmode=transparent width="30" height="30" align="" TYPE="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer"></embed></object></span>`;
   };
   var generate = function generate(_passedURL) {
     var urlToParse = url;
+
     if (_passedURL) {
       urlToParse = _passedURL;
     }
     var html = void 0;
+
     // Find the type of processor to use based on various parts of a URL
-    if (urlToParse.match("youtube") || urlToParse.match("youtu.be")) {
+
+    if (urlToParse.match('youtube') || urlToParse.match('youtu.be')) {
       html = youtube(urlToParse);
-    } else if (urlToParse.match("vimeo")) {
+    } else if (urlToParse.match('vimeo')) {
       html = vimeo(urlToParse);
-    } else if (urlToParse.match(".jpg") || urlToParse.match(".png") || urlToParse.match(".gif") || urlToParse.match(".jpeg")) {
+    } else if (urlToParse.match('.jpg') || urlToParse.match('.png') || urlToParse.match('.gif') || urlToParse.match('.jpeg')) {
       html = image(urlToParse);
-    } else if (urlToParse.match(".ogg") || urlToParse.match(".mp3") || urlToParse.match(".wav")) {
+    } else if (urlToParse.match('.ogg') || urlToParse.match('.mp3') || urlToParse.match('.wav')) {
       html = audio(urlToParse);
     } else {
       // When all else fails, at least my it an a href.
       html = link(urlToParse);
     }
+
     return html;
   }; // end generate
 
@@ -131,22 +137,25 @@ module.exports = function (url) {
     get: function get(_url) {
       return generate(_url);
     },
-    parse: function parse(code) {
+    parse(code) {
       // This will loop over the code, break it into a single words
       // Find all Links and do some magic.
 
-      // New Code holder
-      code = code || "";
       // Link Regex
       var exp = /((https?|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/gi;
+
       // code = code.replace(exp);
-      var words = code.split(" ");
+      var words = code.split(' ');
+
       // Prepare New Words
       var newWords = [];
+
       // Loop over each word
-      words.map(function (word, index) {
+
+      words.forEach(function (word) {
         // Match with expression
         var match = word.match(exp);
+
         if (match) {
           // Set the new Word
           newWords.push(generate(match[0]));
@@ -155,10 +164,8 @@ module.exports = function (url) {
           newWords.push(word);
         }
       });
-      // Join all the words
-      code = newWords.join(" ");
 
-      return code;
+      return newWords.join(' ');
     }
   };
 }; // end function
